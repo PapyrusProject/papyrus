8be2fe1547fa941a7a6edcd852240cb942704be7
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-23 19:51:45 -0300

    add: add about page

---
+++ src/app/About/AboutstatusItems.tsx
@@ -0,0 +1,90 @@
+import { ReactNode } from "react";
+import { PiPencilLineBold, PiPaintBrushBold, PiBooks, PiDatabaseBold, PiDevices, PiHourglassHighBold, PiWrenchBold, PiStrategy } from "react-icons/pi";
+
+
+export type IProps = {
+    id?: number,
+    icon?: ReactNode,
+    title?: String,
+    paragraph?: string
+}[]
+
+const itens: IProps = [
+    {
+        id: 1,
+        icon: <PiPencilLineBold />,
+        title: 'Writing and Editing',
+        paragraph: "Create and enhance content effectively in a simplified editing environment."
+    },
+    {
+        id: 2,
+        icon: <PiPaintBrushBold />,
+        title: "Customization",
+        paragraph: "Customize the system according to your needs and preferences."
+    },
+    {
+        id: 3,
+        icon: <PiBooks />,
+        title: "Organization",
+        paragraph: "Keep information and tasks organized, enabling quick and efficient access."
+    },
+
+    {
+        id: 4,
+        icon: <PiStrategy />,
+        title: "Planning",
+        paragraph: "Effortlessly create and track deadlines and tasks for projects and goals."
+    },
+
+    {
+        id: 5,
+        icon: <PiDatabaseBold />,
+        title: "Database",
+        paragraph: "Store and manage information in a structured manner for easy reference."
+    },
+
+    {
+        id: 6,
+        icon: <PiDevices />,
+        title: "All Devices",
+        paragraph: "Access your information and work from anywhere, on all devices."
+    },
+
+    {
+        id: 7,
+        icon: <PiHourglassHighBold />,
+        title: "Progress Tracking",
+        paragraph: "Visualize your progress clearly, maintaining focus and motivation."
+    },
+
+    {
+        id: 8,
+        icon: <PiWrenchBold />,
+        title: "Reference Organization",
+        paragraph: "Keep all research resources well-organized and accessible."
+    },
+]
+
+export default function AboutStatusItems() {
+    return (
+        <>
+            {itens.map(
+                (i) => {
+                    return (
+                        <div key={i.id} className="flex flex-col items-center justify-center w-64 gap-4 text-center">
+                            <div className="flex items-center justify-center w-20 h-20 rounded-full bg-primary">
+                                <div className="text-5xl text-white">
+                                    {i.icon}
+                                </div>
+                            </div>
+                            <h3 className="font-bold">{i.title}</h3>
+                            <p>
+                                "{i.paragraph}"
+                            </p>
+                        </div>
+                    )
+                }
+            )}
+        </>
+    )
+}
---
+++ src/app/About/CardAbout.tsx
@@ -0,0 +1,55 @@
+import { ReactNode } from "react"
+import { PiArticleBold, PiLightbulbFilamentBold, PiPaletteBold } from "react-icons/pi"
+
+export type IProps = {
+    id?: number,
+    icon?: ReactNode,
+    title?: string,
+    paragraph?: string
+}[]
+
+const itens: IProps = [
+    {
+        id: 1,
+        icon: <PiLightbulbFilamentBold />,
+        title: "Continuous Innovation",
+        paragraph: "Incorporating new ideas and technologies as they emerge"
+    },
+    {
+        id: 2,
+        icon: <PiArticleBold />,
+        title: "Facilitating Creativity",
+        paragraph: "We want to simplify the paper creation process."
+    },
+    {
+        id: 3,
+        icon: <PiPaletteBold />,
+        title: "Functionality and Beauty",
+        paragraph: "Functionality with an attractive interface"
+    },
+
+]
+
+export default function CardAbout() {
+    return (
+        <>
+            {itens.map(
+                (i) => {
+                    return (
+                        <div key={i.id} className="flex flex-col items-center gap-4 px-6 py-20 bg-white w-[90%] xsm:w-96 h-80 rounded-xl">
+                            <div className="text-primary text-7xl">
+                                {i.icon}
+                            </div>
+                            <h2 className="text-2xl font-semibold">{i.title}</h2>
+
+                            <p className="text-center">
+                                {i.paragraph}
+                            </p>
+
+                        </div>
+                    )
+                }
+            )}
+        </>
+    )
+}
