d66ec6fb4b2060d262b08de562f33789e0a31f0e
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-24 16:42:26 -0300

    add: config next ui in tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,14 @@
 import type { Config } from "tailwindcss";
 
 //TODO Escolher cores para borda, input e DARK MODE
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -28,6 +30,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 export default config;
