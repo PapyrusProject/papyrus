a18503817c9dcb04289f3b28d572fc86420c0a76
ggualbertosouza <contatoguilhermegsz@gmail.com>
2023-10-24 16:48:44 -0300

    add: sessions in profile component, styled the header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import { ProfileUnauthorized } from "@/components/profile/ProfileUnauthorized";
+import { ProfileUnauthorized } from "@/components/profile/Profile";
 import Link from "next/link";
 import { Button } from "../ui/Button";
 
 export default function Header() {
   return (
-    <section className="flex items-center justify-between">
-      <h2 className="text-2xl">Papy<span className="text-primary">rus.</span></h2>
-      <div>
-        <Link href='/about'>About</Link>
-        <Link href='/contact'>Contact</Link>
+    <section className="flex items-center justify-between py-2">
+      <h2 className="text-2xl font-bold">Papy<span className="text-primary">rus.</span></h2>
+      <div className="flex item-center gap-2">
+        <Link href='/about' className="hover:text-primary">About</Link>
+        <Link href='/contact' className="hover:text-primary">Contact</Link>
       </div>
       <ProfileUnauthorized />
     </section>
