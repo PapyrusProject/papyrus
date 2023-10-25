2f627bedbb8bbf55bb863e96949ba065c078b699
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-25 18:44:29 -0300

    feat: add login form in login modal

--- src/components/Modal/LoginModal.tsx
+++ src/components/Modal/LoginModal.tsx
@@ -5,6 +5,7 @@ import { useLogin } from "@/store/useLogin";
 
 //modal
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/react";
+import { LoginForm } from "../form/formLogin/LoginForm";
 
 export default function LoginModal() {
 
@@ -14,8 +15,10 @@ export default function LoginModal() {
         <Modal isOpen={show} onOpenChange={setShow}>
             <ModalContent>
                 <ModalHeader onClick={() => setShow()}></ModalHeader>
-                <ModalBody>
-                    <p>Login</p>
+                <ModalBody className="flex flex-col justify-center items-center">
+                    <h1 className="text-primary text-4xl font-bold">Papyrus</h1>
+                    <h2 className="font-semibold text-2xl">Login</h2>
+                    <LoginForm />
                 </ModalBody>
                 <ModalFooter></ModalFooter>
             </ModalContent>
