79956a873898b0d70d77678995ca004256ee8b76
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-25 18:57:48 -0300

    fix: remove forms from profile page

--- src/app/(application)/profile/page.tsx
+++ src/app/(application)/profile/page.tsx
@@ -4,16 +4,9 @@ import { RegisteForm } from "@/components/form/formRegister/RegisterForm";
 
 export default function Profile() {
 
-    return(
+    return (
         <div className="flex items-center justify-center gap-12 w-full">
-            <div>
-                <h2>LOGIN</h2>
-         <LoginForm />
-            </div>
-            <div>
-                <h2>Registro</h2>
-                <RegisteForm />
-            </div>
+
         </div>
     )
 }
