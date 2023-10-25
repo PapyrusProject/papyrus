9dd638f5548bc400595395b1c75c97bec4f465d7
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-25 18:49:56 -0300

    feat: add register form in signup modal

--- src/components/Modal/SignUpModal.tsx
+++ src/components/Modal/SignUpModal.tsx
@@ -5,6 +5,7 @@ import { useSignup } from "@/store/useSignup";
 
 //modal
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/react";
+import { RegisteForm } from "../form/formRegister/RegisterForm";
 
 export default function SignupModal() {
 
@@ -14,8 +15,9 @@ export default function SignupModal() {
         <Modal isOpen={show} onOpenChange={setShow}>
             <ModalContent>
                 <ModalHeader onClick={() => setShow()}></ModalHeader>
-                <ModalBody>
-                    <p>Signup</p>
+                <ModalBody className="flex flex-col justify-center items-center">
+                    <h1 className="text-primary text-4xl font-bold">Sign Up</h1>
+                    <RegisteForm />
                 </ModalBody>
                 <ModalFooter></ModalFooter>
             </ModalContent>
