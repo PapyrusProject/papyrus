a8fe62ae141e0b922143f20d733c2a2dbe31e71a
Pedro Cardoso da Silva <pcsilvamaster@gmail.com>
2023-10-26 14:29:00 -0300

    fix: refactoring homepage, remove navigation page and added 'click here' to about page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,7 @@ import Link from "next/link";
 export default function Header() {
   return (
     <section className="flex items-center justify-between py-2">
-      <Link href='/' className="text-2xl font-bold">Papy<span className="text-primary">rus.</span></Link>
-      <div className="flex item-center gap-2">
-        <Link href='/about' className="hover:text-primary">About</Link>
-        <Link href='/contact' className="hover:text-primary">Contact</Link>
-      </div>
+      <Link href='/' className="text-4xl font-bold">Papy<span className="text-primary">rus.</span></Link>
       <ProfileUnauthorized />
     </section>
   );
