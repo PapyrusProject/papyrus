fcd7cdf3d33ced3ba95eb75ccf32088783a68aa7
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2023-10-27 11:22:56 -0300

    feat: finish footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,24 +2,20 @@
 
 import { useContact } from "@/store/useContact";
 import Link from "next/link";
-import { PiGithubLogoFill, PiDiscordLogoFill } from "react-icons/pi";
+import { RiSendPlaneFill } from 'react-icons/ri'
 
 export default function Footer() {
 
     const setContact = useContact((state) => state.toggleContact)
 
     return (
-        <div className="flex justify-center items-center gap-4">
-            <p>
-                Copyright@ papyprus
-            </p>
-            <Link href='https://github.com/PapyrusProject/papyrus'>
-                <PiGithubLogoFill className="text-primary text-4xl" />
-            </Link>
-            <Link href="https://discord.gg/QcraJAg9">
-                <PiDiscordLogoFill className="text-primary text-4xl" />
-            </Link>
-            <button className="text-primary hover:underline" onClick={() => setContact()}>Contact</button>
+        <div className="flex justify-end items-center gap-4 w-full py-6">
+            <div className="flex justify-between w-[55%]">
+                <p>
+                    Copyright@ papyprus
+                </p>
+                <button className="text-primary flex items-center justify-center gap-2 hover:underline" onClick={() => setContact()}>Contact <RiSendPlaneFill className="text-primary text-xl" /></button>
+            </div>
         </div>
     )
 }
--- src/components/Modal/ContactModal.tsx
+++ src/components/Modal/ContactModal.tsx
@@ -2,6 +2,9 @@
 
 import { useContact } from "@/store/useContact";
 import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter } from "@nextui-org/react";
+import Link from "next/link";
+import { PiDiscordLogoFill, PiGithubLogoFill } from "react-icons/pi";
+import { Button } from "../ui/Button";
 import { Input } from "../ui/Input";
 
 export default function ContactModal() {
@@ -29,9 +32,17 @@ export default function ContactModal() {
                         <textarea placeholder="Type your message..."
                             className="p-2 border border-primary outline-none rounded-xl focus:border-blue-400"
                         ></textarea>
+                        <Button variant='blue' size="md">Send your message</Button>
                     </form>
                 </ModalBody>
-                <ModalFooter></ModalFooter>
+                <ModalFooter className="flex justify-center items-center gap-3">
+                    <Link href='https://github.com/PapyrusProject/papyrus'>
+                        <PiGithubLogoFill className="text-primary text-4xl" />
+                    </Link>
+                    <Link href="https://discord.gg/QcraJAg9">
+                        <PiDiscordLogoFill className="text-primary text-4xl" />
+                    </Link>
+                </ModalFooter>
             </ModalContent>
         </Modal >
     )
